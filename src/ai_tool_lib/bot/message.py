a48c90b04d29c92664c78f9f53fc5500fd56504a
Nathan Ogden <nathan@ogden.tech>
2024-10-29 11:33:54 -0400

    added tests

--- src/ai_tool_lib/bot/message.py
+++ src/ai_tool_lib/bot/message.py
@@ -6,6 +6,8 @@ from pydantic import BaseModel
 
 
 class BotMessageRole(StrEnum):
+    """Message role/sender."""
+
     SYSTEM = "system"
     USER = "user"
     BOT = "assistant"
@@ -14,15 +16,29 @@ class BotMessageRole(StrEnum):
 
 
 class BotToolMessage(BaseModel):
+    """Tool call request message."""
+
     id: str
+    """ Tool call request ID. """
+
     name: str
+    """ Tool name. """
+
     args: str
+    """ Tool call arguments. """
 
 
 class BotMessage(BaseModel):
     """A message to send to the bot."""
 
     role: BotMessageRole
+    """ Message role/sender. """
+
     content: str | None
+    """ Message content. """
+
     tool_calls: list[BotToolMessage] | None = None
+    """ List of tool call requests. """
+
     tool_call_id: str | None = None
+    """ Tool call request ID. """
